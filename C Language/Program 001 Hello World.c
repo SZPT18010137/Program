f53dcee3c5b0dec40be71b61cb7d3a3e120ebed4
咸鱼哥 <62180616+SZPT18010137@users.noreply.github.com>
2021-05-12 18:25:12 +0800

    Update Program 001 Hello World.c

--- C Language/Program 001 Hello World.c
+++ C Language/Program 001 Hello World.c
@@ -1,3 +1,27 @@
-/* 
+/***********************************************************************************
+                          Program 001 Hello World
+Create Date  : May 12, 2021
+Author       : Wu Chu Peng
+Description  : In this program, we will print a string to console, for example like 
+              "Hello, world!".
+Version      : 1.0
+Function List: None
+***********************************************************************************/
 
-*/
+/***********************************************************************************
+                              Header files
+***********************************************************************************/
+#include <stdio.h>
+
+/*************************************************
+Function    : main
+Description : the main function in this program
+Input       : None
+Return      : 0
+*************************************************/
+int main(void)
+{
+  printf("Hello, world!"); // 
+  
+  return 0;
+}
